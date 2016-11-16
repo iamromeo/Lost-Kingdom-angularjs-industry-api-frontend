b07c47986a6ec655d0c8a78c7306ef1f21f78e1c
Dimitris Havlidis <hello@iamromeo.com>
2016-11-16 11:34:19 +0000

    Adding Routes, Bootstrap, caching for http and tempaltes

--- js/app.js
+++ js/app.js
@@ -1,21 +1,46 @@
-var app = angular.module('LostKingdomAPIApp', [])
+var app = angular.module('LostKingdomAPIApp', ["ngRoute"])
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl : "/templates/home.html",
+    controller: "homeController"
+  })
+  .when("/tradegoods", {
+    templateUrl : "/templates/tradegoods.html",
+    controller: "TradegoodsController"
+  })
+  .when("/about", {
+    templateUrl : "about.html"
+  })
+});
+
+app.config(['$httpProvider', function ($httpProvider) {
+    // enable http caching
+   $httpProvider.defaults.cache = true;
+}])
+
+app.controller('homeController', ['$http', function( $http)  {
+	var self = this;
+}]);
 
 app.controller('TradegoodsController', [ '$http', function( $http ) {
 	var self = this;
-	$http.get('http://api-dev.lostkingdom.net/api/v1/tradegoods')
-		.then(
-			function(response) {
-				self.goods = response.data;
-				self.goodsCount = self.goods.length
-			}
-		);
-	$http.get('http://api-dev.lostkingdom.net/api/v1/tradegoodcategories')
-		.then(
-			function(response) {
-				self.categories = response.data;
-				console.log( response.data )
-			}
-		);
+	if ( !self.goods ) {
+		$http.get('http://api-dev.lostkingdom.net/api/v1/tradegoods')
+			.then(
+				function(response) {
+					self.goods = response.data;
+					self.goodsCount = self.goods.length
+				}
+			);
+		$http.get('http://api-dev.lostkingdom.net/api/v1/tradegoodcategories')
+			.then(
+				function(response) {
+					self.categories = response.data;
+				}
+			);
+	}
 }]);
 
 app.filter('searchFor', function(){
@@ -33,3 +58,26 @@ app.filter('searchFor', function(){
 		return result;
 	};
 });
+
+app.directive('bsActiveLink', ['$location', function ($location) {
+return {
+    restrict: 'A', //use as attribute 
+    replace: false,
+    link: function (scope, elem) {
+        //after the route has changed
+        scope.$on("$routeChangeSuccess", function () {
+            var hrefs = ['/#' + $location.path(),
+                         '#' + $location.path(), //html5: false
+                         $location.path()]; //html5: true
+            angular.forEach(elem.find('a'), function (a) {
+                a = angular.element(a);
+                if (-1 !== hrefs.indexOf(a.attr('href'))) {
+                    a.parent().addClass('active');
+                } else {
+                    a.parent().removeClass('active');   
+                };
+            });     
+        });
+    }
+}
+}]);
